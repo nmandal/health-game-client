0ec840a5a1440171a6af202479e253614ca3a59d
nmandal <nicholas.mandal@gmail.com>
2021-12-01 18:58:35 -0600

    fixing for vercel

--- components/Footer.js
+++ components/Footer.js
@@ -10,7 +10,7 @@ const Footer = () => {
       <div className="border-t border-gray-100 py-10 sm:flex sm:items-center sm:justify-between">
         <div className="flex items-center justify-center text-sm text-gray-500">
           <p>Health Game</p>
-          <p className="ml-3 border-l border-gray-200 pl-3"><a target="_blank" href="https://twitter.com/nick_mandal">@nick_mandal</a></p>
+          <p className="ml-3 border-l border-gray-200 pl-3"><a target="_blank" rel="noreferrer" href="https://twitter.com/nick_mandal">@nick_mandal</a></p>
         </div>
         <p className="mt-6 text-sm text-gray-500 text-center sm:mt-0">&copy; 2021 Nick Mandal.</p>
       </div>
